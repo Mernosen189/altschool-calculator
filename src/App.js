aaf43616eafe397cef44e2320f8ca1b13ae81ad9
18-09-1980_sep <mernosen@gmail.com>
2022-10-08 14:21:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from 'react';
+import Screen from './Screen'
+
+
+
 
 function App() {
+ const [calc, setCalc] = useState("")
+ const [result, setResult] = useState("")
+
+ const ops =  ["/", "*", ".","-", "+"]
+
+  let digs = []
+  for(let i = 1; i < 10; i++){
+    digs.push(<button key={i} onClick={()=>updateCalc(i)}>{i}</button>)
+  }
+
+const updateCalc = value => {
+  if (ops.includes(value) && calc === "" || ops.includes(value) && ops.includes(calc.slice(-1)) ){
+    return;
+  }
+  setCalc(calc + value)
+}
+
+const displayRes = () =>{
+
+  let res = eval(calc).toString()
+  setCalc(res)
+
+}
+
+const del = () => {
+  let val = calc.slice(0, -1)
+  setCalc(val.toString())
+}
+
+const reset = () => {
+  setCalc("")
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <div className="calculator">
+        <Screen calc={calc}/>
+       
+        <div className="operators">
+            <button onClick={()=>updateCalc('/')}>/</button>
+            <button onClick={()=>updateCalc('*')}>*</button>
+            <button onClick={()=>updateCalc('-')}>-</button>
+            <button onClick={()=>updateCalc('+')}>+</button>
+            <button onClick={()=>del()} onDoubleClick={()=>reset()}>DEL</button>
+        </div>
+        <div className="digits">
+            {digs}
+            <button onClick={()=>updateCalc('0')}>0</button>
+            <button onClick={()=>updateCalc('.')}>.</button>
+            <button onClick={()=>displayRes()}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
